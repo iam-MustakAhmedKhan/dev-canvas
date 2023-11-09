1bc7342b0a424558940372f1e464fed408e8a890
iam-MustakAhmedKhan <mustakahmedkhan32@gmail.com>
2023-11-09 14:37:18 +0600

    some bug fixes

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
           storageKey="devCanvasTheme"
         >
-          {/* <NextTopLoader /> */}
+          <NextTopLoader />
           <div className="container mx-auto">
             <Header />
             {children}
