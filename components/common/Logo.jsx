352a4b9031892334ce4be1618ae7a78d0ae5f5fd
iam-MustakAhmedKhan <mustakahmedkhan32@gmail.com>
2023-11-09 14:02:16 +0600

    bug fixes

--- components/common/Logo.jsx
+++ components/common/Logo.jsx
@@ -1,14 +1,16 @@
+'use client';
+
 import Link from "next/link";
 
-const Logo = ({bg,text,subTextColor}) => {
-  return (
-      <Link href="/" prefetch={true} replace={false} className={`uppercase font-bold flex items-center text-[${subTextColor?subTextColor:''}] text-[24px]`}>
-          dev
-          <span className={`${bg ? `bg-[${bg}]` : "bg-[#181818]"} dark:bg-[#ffffff] text-[${text ? text :'#ffffff'}] dark:text-[#181818] px-[6px] ml-1  leading-normal rounded-md`}>
-              canvas
-          </span>
-      </Link>
-  )
-}
+const Logo = ({ bg, text, subTextColor }) => {
+    return (
+        <Link href="/" className={`uppercase font-bold flex items-center text-[${subTextColor ? subTextColor : ''}] text-[24px]`}>
+            dev
+            <span className={`${bg ? `bg-[${bg}]` : "bg-[#181818]"} dark:bg-[#ffffff] text-[${text ? text : '#ffffff'}] dark:text-[#181818] px-[6px] ml-1  leading-normal rounded-md`}>
+                canvas
+            </span>
+        </Link>
+    );
+};
 
-export {Logo}
+export { Logo };
